409e749a107863e9c32cf1574062bc973bebbb1b
enil1009 <enilclaros1991@gmail.com>
2022-06-10 13:58:19 -0700

    committed

---
+++ enil.js
@@ -0,0 +1,11 @@
+
+let sum = (2+2);
+console.log(sum);
+
+let bird = function  () {
+    let eagle = "Hi, how are you today?";
+    const bluebird = eagle.split("", 3);
+    console.log(bluebird);
+}
+bird();
+
